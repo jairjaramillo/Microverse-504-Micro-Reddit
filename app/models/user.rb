e25ddd6cfcc67749df47f6803c3e0b46aa3bbe49
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-11-21 16:16:26 -0600

    adding name and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  before_save :format_mail
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 150 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  # alternative: validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } Devise.email_regexp
+
+  private
+
+  def format_mail
+    self.email = email.downcase
+  end
 end
