f6c343f7419dd2c64dd0fb893ebe06b3eda06dca
sarah <sarachamorro@gmail.com>
2019-11-25 11:54:07 -0300

    Delete unneded validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :user_id, presence: true
-  validates :post_id, presence: true
   validates :body, presence: true, length: { maximum: 500 }
 end
